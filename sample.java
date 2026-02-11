b0f3fc5b20ed594afbf975974ff1993ded386f83
Chandralekha KV <kvpchandralekha224@gmail.com>
2026-02-11 21:03:30 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,6 +2,7 @@ class sample{
 	public static void main(String[] args){
 		System.out.println("Hello from DevOps");
 		System.out.println("Hello from Jenkins");
+		System.out.println("Hello from Git");
 
 }
 }
